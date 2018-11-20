fcdc711e028c1cfc533cd12a6e8d7ea60fe8dce7
esle <essi.lehtola@hotmail.com>
2018-11-20 12:55:19 +0200

    nyt ei pitäis näkyä piilotettuja

--- JourneyNotesAPI/Controllers/PersonController.cs
+++ JourneyNotesAPI/Controllers/PersonController.cs
@@ -17,7 +17,7 @@ namespace JourneyNotesAPI.Controllers
         [HttpGet]
         public IEnumerable<string> GetPersons()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "valueX", "valueY" };
         }
 
         // GET: api/Person/5
