ce863ecb8c8d69ccc30b9192a82d19f11cc3af1d
emmaTu <emma.n.tuovinen@gmail.com>
2018-11-28 13:13:42 +0200

    merge fixi

--- JourneyNotesAPI/Controllers/PitstopsController.cs
+++ JourneyNotesAPI/Controllers/PitstopsController.cs
@@ -206,11 +206,9 @@ namespace JourneyNotesAPI.Controllers
         // DELETE: api/ApiWithActions/5
         //[HttpDelete("{TripId}", Name = "TripId")]
         [HttpDelete("{TripId}/{PitstopId}")]
-<<<<<<< HEAD
-        public async Task<ActionResult<string>> DeletePitstop(int TripId, int PitstopId)
-=======
+
         public async Task<ActionResult<string>> DeletePitstop([FromRoute] int TripId, int PitstopId)
->>>>>>> master
+
         {
             string UserID = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
             //string UserID = "666";
